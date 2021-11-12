b8e876bc2981b633384ba9e6c8bfea6fb0eee171
kangcritical <93913625+kangcritical@users.noreply.github.com>
2021-11-13 04:46:39 +0700

    users page
    coba2 alert box di express

--- tes/controllers/usersController.js
+++ tes/controllers/usersController.js
@@ -7,33 +7,26 @@ let getUsersPage = (req, res) => {res.render('users',{
         req.user.anggota3,
         req.user.anggota4,
         req.user.anggota5
-    ]
+    ],
+    pesan : req.flash('tahap2')
 })}
 
 let submitTahap2 = (req,res) => {
-    let data = {
-        user : req.user,
-        anggota : [
-            req.user.anggota1,
-            req.user.anggota2,
-            req.user.anggota3,
-            req.user.anggota4,
-            req.user.anggota5
-        ]
-    }
 
     let kis = {
         type : req.files.kis.mimetype,
-        size : req.files.kis.size,
+        size : req.files.kis.size
     }
 
-    let abc = req.files.kis
-
-    if (kis.type === 'image/png' || kis.type === 'image/jpg' || kis.type === 'image/jpeg' || kis.type === 'application/pdf') {
-        console.log(__dirname)
-    }else{
-        console.log('extensi file anda bukan png, jpg, jpeg, pdf')
+    if (!req.files || Object.keys(req.files).length === 0) {
+        req.flash('tahap2', 'isi')
+        return res.redirect('users')
+    }else if (kis.type === 'image/png' || kis.type === 'image/jpg' || kis.type === 'image/jpeg' || kis.type === 'application/pdf') {
+        req.flash('tahap2', 'true')
+        res.redirect('users')
     }
+
+    console.log('hai')
     // if (kis.size ) {}
 
     
