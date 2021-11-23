286ce79a858db2085c76d322d99fa2406dcf68f6
kangcritical <93913625+kangcritical@users.noreply.github.com>
2021-11-23 11:20:34 +0700

    bug submit
    GK BISA DI PENCET 2KALI COOOK

--- tes/public/js/usersModal.js
+++ tes/public/js/usersModal.js
@@ -2,11 +2,16 @@ let open = document.getElementById('op-modal')
 let modal = document.getElementById('modal')
 let close1 = document.getElementsByClassName('close')[0]
 let close2 = document.getElementsByClassName('closeb')[0  ]
+let submit = document.getElementById('submit-modal')
 
 open.onclick = function() {
     modal.style.display = "block";
   }
   
+submit.onclick = function() {
+  modal.style.display = "none";
+}
+
 close1.onclick = function() {
     modal.style.display = "none";
   }
