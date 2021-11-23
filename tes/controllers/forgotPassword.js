602c5a1a610de60cc88d3a4825b6a3894abde5c4
kangcritical <93913625+kangcritical@users.noreply.github.com>
2021-11-23 11:21:11 +0700

    FORGET PASSWORD
    ON PROGRESS

--- tes/controllers/forgotPassword.js
+++ tes/controllers/forgotPassword.js
@@ -18,21 +18,26 @@ let postForget = (req,res) => {
             console.log(err)
             return res.status(404).send(`<h1 style="margin:50px auto;padding: 10px;width:80%;background-color: red;color:white;text-align:center;border-radius:10px;">error</h1>`)
         }else{
+
+            if (result[0].status === 0) {
+                req.flash('msgForgot','Akun belum di verifikasi')
+                return res.redirect('/forgot-password')            }
+
             if (result.length > 0) {
 
                 let token = jwt.sign(
-                    {username : result[0].username, id : result[0].id},
+                    {email : result[0].email, id : result[0].id},
                     process.env.TOKEN_FORGOTPASS,
                     {expiresIn : '10m'}
                 )
 
                 let mailOption = {
                     from : process.env.MAIL,
-                    to : `${email}`,
-                    subject : `Account Reset Password`,
+                    to : `${req.body.email}`,
+                    subject : `Account Verification`,
                     html : `<h1>Halo Peserta ARESTA 17</h1><br><hr>
                     <p>kami dari panitia ARESTA17 ingin gk tau ngapain</p>
-                    <a href="http://localhost:3000/reset-password/?verify=${token}">KLIK DI SINI UNTUK Reset Password</a>`
+                    <a href="http://localhost:3000/reset-password/?verify=${token}">KLIK DI SINI UNTUK KONFIRMASI AKUN</a>`
                 }
 
                 transporter.sendMail(mailOption,(err, info) => {
@@ -40,9 +45,7 @@ let postForget = (req,res) => {
                         console.log(err)
                         return res.status(404).send(`<h1 style="margin:50px auto;padding: 10px;width:80%;background-color: red;color:white;text-align:center;border-radius:10px;">error</h1>`)
                     }else{
-                        res.cookie("userForget",email,{
-                            maxAge : 1 * 1 * 10 * 60 * 1000
-                        })
+                        console.log(info)
 
                         return res.send(`<h1 style="margin:50px auto;padding: 10px;width:80%;background-color: #0db02b;color:white;text-align:center;border-radius:10px;">pesan berhasil dikirim ke email anda</h1>`)
 
@@ -61,17 +64,22 @@ let getReset = (req,res) => {
     if (!req.query['verify']) return res.status(404).send('Page Not Found')
 
     let tokenAmbil = req.query['verify']
-    console.log(process.env.TOKEN_FORGOTPASS)
-    const validToken = jwt.verify(tokenAmbil,process.env.TOKEN_FORGOTPASS)        
-    if (validToken) {
-        return res.render('forgetPass',{hal : 'reset'})
-    }else{
-        return res.status(404).send('Page Not Found')
+    try {
+        const validToken = jwt.verify(tokenAmbil,process.env.TOKEN_FORGOTPASS)        
+        if (validToken) {
+            return res.render('forgetPass',{hal : 'reset'})
+        }
+    } catch (error) {
+        console.log(error)
+        return res.status(404).send('Page Not Found')    
     }
 }
 
 let postReset = (req,res) => {
-
+// amanin kolom input
+    let ambil = req.query['verify']
+    const data = jwt.verify(ambil,process.env.TOKEN_FORGOTPASS)
+// ubah password
 }
 
 module.exports = {getForgot,getReset,postForget,postReset}
