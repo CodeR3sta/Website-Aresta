bfb48ed917929fa6649c77595d3f3d5bdb19a43f
kangcritical <93913625+kangcritical@users.noreply.github.com>
2021-11-26 16:41:07 +0700

    ubah session app.js jadi 3 jam

--- tes/app.js
+++ tes/app.js
@@ -1,56 +1,58 @@
-const express = require("express")
-const flash = require("connect-flash")
-const path = require('path')
-const cookieParser = require('cookie-parser')
-const session = require('express-session')
-const passport = require('passport')
-const {initWebRoutes} = require('./routes/index')
-const fileUpload = require('express-fileupload')
+const express = require("express");
+const flash = require("connect-flash");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const passport = require("passport");
+const { initWebRoutes } = require("./routes/index");
+const fileUpload = require("express-fileupload");
 
-const app = express()
+const app = express();
 
 // FILE UPLOADER
-app.use(fileUpload())
+app.use(fileUpload());
 
-// PUBLIC 
-const publicDirectory = path.join(__dirname, './public')
-app.use(express.static(publicDirectory))
+// PUBLIC
+const publicDirectory = path.join(__dirname, "./public");
+app.use(express.static(publicDirectory));
 
 // parse Url encoded bodies (as sent by HTML forms)
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }));
 
 // use Cookie Parser
-app.use(cookieParser('secret'))
+app.use(cookieParser("secret"));
 
 // config session
-app.use(session({
-    secret: 'secret',
+app.use(
+  session({
+    secret: "secret",
     resave: true,
     saveUninitialized: false,
     cookie: {
-        maxAge : 1000 * 60 * 60 * 24
-    }
-}));
+      maxAge: 1000 * 60 * 3 * 1 * 1,
+    },
+  })
+);
 
 // enable Flash message
-app.use(flash())
+app.use(flash());
 
 // VIEWS
-app.set('view engine', 'ejs')
+app.set("view engine", "ejs");
 
 // conficg passport middleware
-app.use(passport.initialize())
-app.use(passport.session())
+app.use(passport.initialize());
+app.use(passport.session());
 
 // init all Webroute
-initWebRoutes(app)
+initWebRoutes(app);
 
-app.use('/', (req, res) => {
-    res.status(404)
-    res.send(`NOT FOUND`)
-})
+app.use("/", (req, res) => {
+  res.status(404);
+  res.send(`NOT FOUND`);
+});
 
-const port = 3000
+const port = 3000;
 app.listen(port, () => {
-    console.log(`server is listening on port ${port}`)
-})
+  console.log(`server is listening on port ${port}`);
+});
