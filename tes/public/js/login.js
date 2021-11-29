fba303bd673586bc44af47cf054a309e055e59b4
kangcritical <93913625+kangcritical@users.noreply.github.com>
2021-11-29 20:53:13 +0700

    login page show pass revisi

--- tes/public/js/login.js
+++ tes/public/js/login.js
@@ -1,10 +1,9 @@
-
-function showPass() {
-    const show = document.getElementById('show')
-    const pass = document.getElementById('password')
-    if (show.checked) {
-        pass.type = 'text'
-    }else{
-        pass.type = 'password'
-    }
-}
+document.getElementById("show").addEventListener("change", () => {
+  const show = document.getElementById("show");
+  const pass = document.getElementById("password");
+  if (show.checked) {
+    pass.type = "text";
+  } else {
+    pass.type = "password";
+  }
+});
