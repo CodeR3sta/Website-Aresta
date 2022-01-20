f50920ff13642a806eda8e8430619f761ec3dc8f
Faralha <69440085+Faralha@users.noreply.github.com>
2022-01-20 17:14:01 +0700

    nice

---
+++ js/music-list.js
@@ -0,0 +1,38 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Aresta17 - Harimu",
+    artist: "B.O.R.N",
+    img: "Aresta17-Harimu",
+    src: "Aresta17-Harimu"
+  },
+  {
+    name: "Aresta17 - Humaniora",
+    artist: "B.O.R.N",
+    img: "Aresta17-Humaniora",
+    src: "Aresta17-Humaniora"
+  },
+  {
+    name: "Aresta17 - Hilang",
+    artist: "B.O.R.N",
+    img: "mAresta17-Hilang",
+    src: "Aresta17-Hilang"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
